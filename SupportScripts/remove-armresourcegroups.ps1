92ed6b83119ffcfdf067502efdc794f9fd273caa
Fabian Flanhardt <fabian.flanhardt@ceterion.com>
2017-12-16 12:21:58 +0100

    Test-Group added

--- SupportScripts/remove-armresourcegroups.ps1
+++ SupportScripts/remove-armresourcegroups.ps1
@@ -2,3 +2,4 @@ Remove-AzureRmResourceGroup -name webservers -force
 Remove-AzureRmResourceGroup -name Monitoring -force
 Remove-AzureRmResourceGroup -name Network -force
 Remove-AzureRmResourceGroup -name securitydata -force
+Remove-AzureRmResourceGroup -name ff-meetup-test -force
