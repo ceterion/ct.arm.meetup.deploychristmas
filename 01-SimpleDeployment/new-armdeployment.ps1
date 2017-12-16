045906c795700cc4e702d852051b99c2bde408e2
Fabian Flanhardt <fabian.flanhardt@ceterion.com>
2017-12-16 12:16:42 +0100

    Comments added

--- 01-SimpleDeployment/new-armdeployment.ps1
+++ 01-SimpleDeployment/new-armdeployment.ps1
@@ -1,2 +1,5 @@
+# A Resource group must be created prior to starting a corresponding deployment 
 New-AzureRmResourceGroup -name "ff-test-01" -Location "westeurope"
+
+# Start my Resource Group deployment
 New-AzureRmResourceGroupDeployment -ResourceGroupName "ff-test-01" -TemplateFile ".\01-SimpleDeployment\deploytemplate.json"
