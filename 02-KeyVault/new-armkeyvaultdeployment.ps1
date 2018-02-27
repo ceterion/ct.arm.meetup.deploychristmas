2554052bdf491713e1c16dbd38c313c9df0f5bfc
Fabian Flanhardt <fabian.flanhardt@ceterion.com>
2018-02-27 11:15:22 +0100

    Script now prompts for Environmentprefix

--- 02-KeyVault/new-armkeyvaultdeployment.ps1
+++ 02-KeyVault/new-armkeyvaultdeployment.ps1
@@ -3,10 +3,11 @@ $resourcegroupname = "securitydata"
 
 $Parameters = New-Object -TypeName Hashtable
 
+$Environmentprefix = Read-Host - Prompt "Provide a Prefix that will be leading the Name of your KeyVault" 
 # Get current AzureContext Account ID 
 $CurrentAccountID = (Get-AzureRmADUser -defaultprofile (Get-AzureRmcontext)).id.tostring()
 $Parameters.Add('AccountID', $CurrentAccountID)
-$Parameters.Add('keyVaultName', 'ff-DeploymentVault')
+$Parameters.Add('keyVaultName', $Environmentprefix + '-DeploymentVault')
 $Parameters.Add('secretname', 'AdminLocal')
 
 New-AzureRmResourceGroup -name $resourcegroupname -Location "westeurope"
