7652defe6963e13b7a0d23a24195f934b68b409b
Fabian Flanhardt <fabian.flanhardt@ceterion.com>
2018-03-01 00:04:33 +0100

    Image to deploy to web servers has been replaced

--- SupportScripts/remove-armresourcegroups.ps1
+++ SupportScripts/remove-armresourcegroups.ps1
@@ -2,4 +2,3 @@ Remove-AzureRmResourceGroup -name webservers -force
 Remove-AzureRmResourceGroup -name Monitoring -force
 Remove-AzureRmResourceGroup -name Network -force
 Remove-AzureRmResourceGroup -name securitydata -force
-Remove-AzureRmResourceGroup -name ff-meetup-test -force
