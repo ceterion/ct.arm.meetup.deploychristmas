3df719bd36e7d6d0cca01a5f41b7f51083c7b047
Fabian Flanhardt <fabian.flanhardt@ceterion.com>
2018-03-02 07:18:22 +0100

    debug switch has been added to deployment command

--- 01-SimpleDeployment/new-armdeployment.ps1
+++ 01-SimpleDeployment/new-armdeployment.ps1
@@ -2,4 +2,4 @@
 New-AzureRmResourceGroup -name "ff-test-01" -Location "westeurope"
 
 # Start my Resource Group deployment
-New-AzureRmResourceGroupDeployment -ResourceGroupName "ff-test-01" -TemplateFile ".\01-SimpleDeployment\deploytemplate.json"
+New-AzureRmResourceGroupDeployment -ResourceGroupName "ff-test-01" -TemplateFile ".\01-SimpleDeployment\deploytemplate.json" -Verbose
